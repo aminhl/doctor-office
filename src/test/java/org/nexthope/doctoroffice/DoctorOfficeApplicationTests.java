a68eaf56f6ffd89ff0bcb388e90383cf9eb0723c
aminhl <amin.hlel@esprit.tn>
2025-05-01 17:16:32 +0200

    feat: create build github-actions pipeline

--- src/test/java/org/nexthope/doctoroffice/DoctorOfficeApplicationTests.java
+++ src/test/java/org/nexthope/doctoroffice/DoctorOfficeApplicationTests.java
@@ -1,13 +1,20 @@
 package org.nexthope.doctoroffice;
 
+import lombok.RequiredArgsConstructor;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
+@RequiredArgsConstructor
 class DoctorOfficeApplicationTests {
+
+    private final ApplicationContext applicationContext;
 
     @Test
     void contextLoads() {
+        Assertions.assertThat(applicationContext).isNotNull();
     }
 
 }

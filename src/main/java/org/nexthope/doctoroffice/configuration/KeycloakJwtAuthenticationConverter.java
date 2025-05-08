b1ebcdf2411ead9dedafa2b4194491c4e23cc7a7
aminhl <amin.hlel@esprit.tn>
2025-05-08 12:02:04 +0200

    feat: set up the spring security and keycloak configuration

---
+++ src/main/java/org/nexthope/doctoroffice/configuration/KeycloakJwtAuthenticationConverter.java
@@ -0,0 +1,49 @@
+package org.nexthope.doctoroffice.configuration;
+
+import lombok.NonNull;
+import org.apache.commons.collections4.MapUtils;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Component
+public class KeycloakJwtAuthenticationConverter implements Converter<Jwt, AbstractAuthenticationToken> {
+
+    @Override
+    public AbstractAuthenticationToken convert(@NonNull Jwt jwt) {
+        return new JwtAuthenticationToken(
+                jwt,
+                Stream.concat(
+                        new JwtGrantedAuthoritiesConverter().convert(jwt).stream(),
+                        extractUserRoles(jwt).stream()
+                ).collect(Collectors.toSet())
+        );
+    }
+
+    /**
+     * Extract resource roles from the {@link Jwt} object and return them into a Set of {@link SimpleGrantedAuthority}
+     * @param jwt the {@link Jwt} object contains the token claims
+     * @return a Set of {@link SimpleGrantedAuthority} objects representing the user roles
+     */
+    private Set<? extends GrantedAuthority> extractUserRoles(@NonNull Jwt jwt) {
+        Map<String, Object> resourceAccess = MapUtils.emptyIfNull(jwt.getClaim("resource_access"));
+        Map<String, List<String>> eternal = (Map<String, List<String>>) resourceAccess.get("account");
+        List<String> roles = eternal.get("roles");
+
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.replace("-", "_")))
+                .collect(Collectors.toSet());
+    }
+
+}
---
+++ src/main/java/org/nexthope/doctoroffice/configuration/SecurityConfiguration.java
@@ -0,0 +1,48 @@
+package org.nexthope.doctoroffice.configuration;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@Slf4j
+public class SecurityConfiguration {
+
+    private final KeycloakJwtAuthenticationConverter keycloakJwtAuthenticationConverter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        log.debug("SecurityFilterChain - START");
+        http.csrf(AbstractHttpConfigurer::disable)
+            .cors(withDefaults())
+            .authorizeHttpRequests(
+                    req -> req.requestMatchers(
+                    "/v3/api-docs",
+                    "/v3/api-docs/**",
+                    "/swagger-resources",
+                    "/swagger-resources/**",
+                    "/configuration/ui",
+                    "/configuration/security",
+                    "/swagger-ui/**",
+                    "/webjars/**",
+                    "swagger-ui.html"
+            )
+                .permitAll()
+                .anyRequest()
+                .authenticated())
+            .oauth2ResourceServer(oauth2 -> oauth2.jwt(token ->
+                    token.jwtAuthenticationConverter(keycloakJwtAuthenticationConverter)));
+        log.debug("SecurityFilterChain - END");
+        return http.build();
+    }
+
+}

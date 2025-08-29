a485e2c3506f4fe76b82f3243b4a4b42830e3b11
NextHope <amin.hlel@esprit.tn>
2025-08-29 21:46:10 +0200

    feat: add openapi and swagger config

---
+++ src/main/java/org/nexthope/doctoroffice/configuration/OpenApiConfiguration.java
@@ -0,0 +1,39 @@
+package org.nexthope.doctoroffice.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfiguration {
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title(applicationName)
+                        .description("API documentation for Doctor's Office Management Application")
+                        .version("v1.0.0")
+                        .contact(new Contact().name("Nexthope")))
+                .servers(List.of(new Server().url("/").description("Default Server URL")));
+    }
+
+    @Bean
+    public GroupedOpenApi apiV1() {
+        return GroupedOpenApi.builder()
+                .group("v1")
+                .pathsToMatch("/api/v1/**")
+                .build();
+    }
+
+}
--- src/main/java/org/nexthope/doctoroffice/configuration/SecurityConfiguration.java
+++ src/main/java/org/nexthope/doctoroffice/configuration/SecurityConfiguration.java
@@ -26,15 +26,7 @@ public class SecurityConfiguration {
             .cors(withDefaults())
             .authorizeHttpRequests(
                     req -> req.requestMatchers(
-                    "/v3/api-docs",
-                    "/v3/api-docs/**",
-                    "/swagger-resources",
-                    "/swagger-resources/**",
-                    "/configuration/ui",
-                    "/configuration/security",
-                    "/swagger-ui/**",
-                    "/webjars/**",
-                    "swagger-ui.html"
+                    "/swagger-ui/**"
             )
                 .permitAll()
                 .anyRequest()
